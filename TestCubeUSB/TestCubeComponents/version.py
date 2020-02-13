63cd614973f4f0cee96b439ee94c39fca24db9db
chris <crsayen@gmail.com>
2020-02-13 15:34:14 -0500

    fixed silly conditional mistake

--- TestCubeUSB/TestCubeComponents/version.py
+++ TestCubeUSB/TestCubeComponents/version.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 
 class VersionMessage:
@@ -7,13 +8,15 @@ class VersionMessage:
         pass
 
     async def getFirmwareVersion(self):
+        print("get ver")
         self.ask = True
+        self.finished()
         if self.getVersionEvent is None:
             self.getVersionEvent = asyncio.Event()
         else:
             self.getVersionEvent.clear()
-        self.finished()
         await self.getVersionEvent.wait()
+        print(f"{self.version}")
         return [{"path": "/version", "data": self.version}]
 
     def get_version_messages(self):
@@ -22,6 +25,8 @@ class VersionMessage:
         return [f"{0x50:08x}"]
 
     def recusb_51_version(self, payload):
+        if self.logger.isEnabledFor(logging.DEBUG):
+            self.logger.debug(f"{payload}")
         if len(payload) < 12:
             self.version = "0.0.0"
         else:
@@ -31,6 +36,6 @@ class VersionMessage:
                 int(payload[8:12], 16),
             )
             self.version = f"{hi}.{lo}.{patch}"
-        if self.getVersionEvent is not None and self.getVersionEvent.is_set():
+        if self.getFirmwareVersion is not None and not self.getVersionEvent.is_set():
             self.getVersionEvent.set()
         return [{"path": "/version", "data": self.version}]
