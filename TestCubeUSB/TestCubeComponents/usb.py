07c8eb9dc01178a67a46e92e845694c7f8f06266
crsayen <crsayen@gmail.com>
2020-02-15 15:49:53 -0500

    Update usb.py

--- TestCubeUSB/TestCubeComponents/usb.py
+++ TestCubeUSB/TestCubeComponents/usb.py
@@ -4,14 +4,11 @@ class UsbMessage:
 
     def sendrawusb(self, msg):
         self.usbmsg = msg
-        print(f"sendrawusb({msg=})")
 
     def get_sendusb_messages(self):
-        print(f"get_sendusb_messages()")
         if self.usbmsg is None:
             return []
         if isinstance(self.usbmsg,str):
-            self.usbmsg = [self.usbmsg]
-        print(f"get_sendusb_messages({self.usbmsg=})")     
+            self.usbmsg = [self.usbmsg] 
         return self.usbmsg
 
