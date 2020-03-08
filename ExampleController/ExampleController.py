65761d55bbd6cfeda131f74f84597843fcdd0b71
Chris Sayen <chrissayen@iMac.local>
2020-03-07 19:59:19 -0500

    more fun

--- ExampleController/ExampleController.py
+++ ExampleController/ExampleController.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from pyGizmoServer.utility import log
 from pyGizmoServer.controller import Controller
 
@@ -8,8 +9,13 @@ class ExampleController(Controller):
         super().__init__(*args, **kw)
         self.counter = 0
         self.strings = ["a", "b", "c"]
-        self.concat = ''.join(self.strings)
-        self.stridx = 0
+        self.read_idx = 0
+
+    """
+    This gets called after the server processes a request.
+    """
+    def finished_processing_request(self):
+        log("someone called me")
 
     """
     Setup gets called once at startup.
@@ -21,14 +27,8 @@ class ExampleController(Controller):
     def setString(self, index, value):
         self.strings[index] = value
 
-    async def getString(self, index):
-        return self.strings[index]
-
-    """
-    This gets called after the server processes a request.
-    """
-    def finished_processing_request(self):
-        log("someone called me")
+    async def getnumber(self):
+        return random.randint(0, 100)
 
     """
     The handler gets called repeatedly.
@@ -36,10 +36,6 @@ class ExampleController(Controller):
     """
     async def handler(self):
         await asyncio.sleep(0.2)
-        self.concat = self.strings[self.stridx]
-        self.stridx += 1
-        self.stridx %= 3
-        self.counter += 1
-        self.counter %= 100
-        self.send("/watch_me_read", self.concat)
-        self.send("/watch_me_count", self.counter)
+        self.read_idx += 1
+        self.read_idx %= 3
+        self.send("/watch_me_read", self.strings[read_idx])
