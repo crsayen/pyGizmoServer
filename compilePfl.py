dc921c5dd11135edf7e1b97acd3df8e78a65ca48
Chris sayen <neuralskill@gmail.com>
2020-04-21 16:35:10 -0400

    continued progress on compiler

--- compilePfl.py
+++ compilePfl.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('C:\\\\dev\\xl.xlsx')
 
-print(df)
-
 channels = list(df.columns.values)
 
 mask = int(''.join(['1' if i in channels  else '0' for i in range(1,13)][::-1]), 2)
@@ -11,8 +9,8 @@ mask = int(''.join(['1' if i in channels  else '0' for i in range(1,13)][::-1]),
 def createAddEntryMessage(channel, flag, small=0, large=0):
     return '{:02x}{:02x}{:02x}{:04x}'.format(channel,flag,small,large)
 
-def createDcEntryMessage(channel, duty, hold):
-    return [createAddEntryMessage(channel, 0, small=duty, large=hold)]
+def createDcEntryMessage(channel, duty):
+    return [createAddEntryMessage(channel, 0, small=duty)]
 
 def createDcRampEntryMessages(channel, start, end, duration):
     return [
@@ -34,3 +32,23 @@ def createFreqRampEntryMessage(channel, start, end, duration):
 
 def createJumpEntryMessage(channel, index):
     return [createAddEntryMessage(channel, 2, index)]
+
+def parseGtv(channel, value):
+    if value[-1] == '%':
+        return createDcEntryMessage(channel, int(value[:-1]))
+    else:
+        return createFreqEntryMessage(channel, int(value[:-2]))
+
+ops = {
+    'gtv': parseGtv,
+    'slp': parseSlp,
+    'jmp': parseJmp
+}
+
+for channel in channels:
+    for line in df[channel].tolist():
+        lineChunks = line.split(' ')
+        f = ops.get(lineChunks[0])
+        if f is None:
+            raise RuntimeError(f'invalid operation: {lineChunks[0]}')
+        f(channel, *lineChunks[1:])
